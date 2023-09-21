952c54c29e21dc4a4865d4d220785612b8112ac7
Lucas Pevidor <lucas.pevidor@gmail.com>
2023-09-21 03:41:36 -0300

    Added more animations

--- src/components/Board/board.tsx
+++ src/components/Board/board.tsx
@@ -1,25 +1,38 @@
 import {
   BoardController,
   BoardItem,
+  Direction,
   GameStatus,
 } from "../../controllers/BoardController";
 import { X, Circle } from "lucide-react";
 
 import "./style.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import BoardTitle from "./BoardTitle/board-title";
 import BoardDescription from "./BoardDescription/board-description";
 
 const bc = new BoardController(3, 3);
 
+function defineWinDirection(dir: Direction) {
+  const [x, y] = dir;
+  if (x === 1 && y === 0) return "horizontal";
+  if (x === 0 && y === 1) return "vertical";
+  if (x === 1 && y === 1) return "diagonalRight";
+  if (x === -1 && y === 1) return "diagonalLeft";
+
+  return "";
+}
+
 const Board = () => {
   const [board, setBoard] = useState(bc.Board);
   const [gameStatus, setGameStatus] = useState(bc.status);
+  const [winPosition, setWinPosition] = useState(bc.winnerPosition);
 
   function handleClick({ rIndex, cIndex }: { rIndex: number; cIndex: number }) {
     bc.Place(rIndex, cIndex);
     setBoard({ ...bc.Board });
     setGameStatus(bc.status);
+    setWinPosition(bc.winnerPosition);
   }
 
   function playAgainClicked() {
@@ -29,24 +42,23 @@ const Board = () => {
       elements.item(i)?.classList.add("despawn");
     }
 
+    document
+      .getElementsByClassName("winLine")
+      .item(0)
+      ?.classList.add("despawn");
+
+    document
+      .getElementsByClassName("playAgainBtn")
+      .item(0)
+      ?.classList.add("despawn");
+
     setTimeout(() => {
       bc.ClearBoard();
       setBoard({ ...bc.Board });
       setGameStatus(bc.status);
+      setWinPosition(bc.winnerPosition);
     }, 250);
   }
-
-  useEffect(() => {
-    if (gameStatus !== GameStatus.X_TURN && gameStatus !== GameStatus.O_TURN) {
-      const elements = document.getElementsByClassName("selectable");
-      console.log("Length", elements.length);
-      for (let i = 0; i < elements.length; i++) {
-        elements.item(i)?.classList.remove("selectable");
-      }
-    }
-
-    console.log("ran");
-  }, [gameStatus]);
 
   return (
     <>
@@ -58,6 +70,16 @@ const Board = () => {
             <tr key={rIndex} style={{ display: "flex" }}>
               {row.map((cell, cIndex) => (
                 <td key={rIndex * 10 + cIndex}>
+                  {winPosition &&
+                    winPosition.row === rIndex &&
+                    winPosition.col === cIndex && (
+                      <div
+                        className={
+                          "winLine " + defineWinDirection(winPosition.direction)
+                        }
+                      ></div>
+                    )}
+
                   <button onClick={() => handleClick({ rIndex, cIndex })}>
                     {cell !== BoardItem.NONE ? (
                       cell === BoardItem.CIRCLE ? (
@@ -82,11 +104,14 @@ const Board = () => {
           ))}
         </tbody>
       </table>
-      {gameStatus !== GameStatus.O_TURN && gameStatus !== GameStatus.X_TURN && (
-        <button className="playAgainBtn" onClick={() => playAgainClicked()}>
-          Play again!
-        </button>
-      )}
+      <div className="playAgainDiv">
+        {gameStatus !== GameStatus.O_TURN &&
+          gameStatus !== GameStatus.X_TURN && (
+            <button className="playAgainBtn" onClick={() => playAgainClicked()}>
+              Play again!
+            </button>
+          )}
+      </div>
     </>
   );
 };
