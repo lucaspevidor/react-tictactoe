69af1709715d3779fe584f054f7029518364feb8
Lucas Pevidor <lucas.pevidor@gmail.com>
2023-09-21 00:54:49 -0300

    Fixed some styling issues

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,15 @@ import Board from "./components/Board/board";
 
 const App = () => {
   return (
-    <div>
+    <div
+      style={{
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <Board />
     </div>
   );
